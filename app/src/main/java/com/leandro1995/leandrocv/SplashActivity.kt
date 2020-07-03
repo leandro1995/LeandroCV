78055cde7dbde4475a0719d546c39e859e84ea7f
leandro1995 <leccbo199@gmail.com>
2020-07-02 20:08:43 -0500

    Validar cuando se acepta los terminos y condiciones

--- app/src/main/java/com/leandro1995/leandrocv/SplashActivity.kt
+++ app/src/main/java/com/leandro1995/leandrocv/SplashActivity.kt
@@ -3,8 +3,12 @@ package com.leandro1995.leandrocv
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.leandro1995.leandrocv.activity.TermAndConditionActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.leandro1995.leandrocv.config.Setting
+import com.leandro1995.leandrocv.databinding.ActivitySplashBinding
+import com.leandro1995.leandrocv.viewmodel.SplashViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -12,15 +16,42 @@ import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashBinding: ActivitySplashBinding
+    private lateinit var splashViewModel: SplashViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
 
+        splashViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+
+        observer()
+
+        splashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        splashBinding.splashModel = splashViewModel
+
+        view()
+    }
+
+    private fun view() {
         GlobalScope.launch(Dispatchers.Main) {
             delay(TimeUnit.SECONDS.toMillis(Setting.SPLASH_TIME))
 
-            startActivity(Intent(this@SplashActivity, TermAndConditionActivity::class.java))
-            finish()
+            splashViewModel.splashTimeOut()
+        }
+    }
+
+    private fun observer() {
+        splashViewModel.apply {
+            termAndConditionMutable.observe(this@SplashActivity, Observer {
+                startActivity(Intent(this@SplashActivity, it))
+                finish()
+            })
+
+            informationMutable.observe(this@SplashActivity, Observer {
+                startActivity(Intent(this@SplashActivity, it))
+                finish()
+            })
         }
     }
 }
